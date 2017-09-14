f4d11c730c7bf8353d6ba7bf88060ff053074859
Ole-Martin Skagevang <hero3@hotmail.com>
2017-09-14 13:41:03 +0200

    semicolons

--- public/js/coffeemaker.js
+++ public/js/coffeemaker.js
@@ -1,5 +1,5 @@
-const coffee_per_cup = 7.5
-const water_per_cup = 12.5
+const coffee_per_cup = 7.5;
+const water_per_cup = 12.5;
 
 function calculate_coffee(n_cups) {
   let coffee = coffee_per_cup * n_cups;
@@ -12,5 +12,5 @@ $('#nCups').on('input', function() {
   let res = calculate_coffee(cups);
   $("#result").html("<div>You need:<br/>" +
     res.coffee + " grams of coffee<br/>" +
-    res.water + " ml of water")
+    res.water + " ml of water");
 });
