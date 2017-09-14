c2c44bd6cff22c9651beb0fcade5ec8136d1e9dd
Andreajl <andreajl@stud.ntnu.no>
2017-09-14 14:09:40 +0200

    bugfix

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,21 @@
+$(document).on('load', function() {
+  addMultipleImages();
+});
+
 $(document).on('scroll', function(){
   if ($(document).scrollTop() > $('#posters').height() - $('#first').height()/8) {
-    let count = ($(document).width()/2) / 200;
-    for (i = 0; i < count - 1; i++) {
-      addImage();
-    }
+    addMultipleImages();
   }
 });
 
-function addImage() {
+function addMultipleImages() {
+  let count = ($(document).width()/2) / 200;
+  for (i = 0; i < count - 1; i++) {
+    addSingleImage();
+  }
+}
+
+function addSingleImage() {
   let number = Math.floor((Math.random() * 15) + 1);
   let delay = Math.floor((Math.random() * 1000) + 500);
   let img = $('<img class="child hide grow" src="../img/posters/poster' + number + '.jpg" />');
