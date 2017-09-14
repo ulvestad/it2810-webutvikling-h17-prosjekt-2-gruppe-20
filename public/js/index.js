4ba78d80af3450623ce70c18d18953d9e93a903d
Andreajl <andreajl@stud.ntnu.no>
2017-09-14 13:44:29 +0200

    homepage now uses jquary, implement lazy load (unlimited posters)

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,16 @@
-let posters = 15;
-let done = 1;
-
-window.onload = function() {
-  setInterval(function() {
-    changeImage();
-  }, 3000);
-}
-
-window.onscroll = function() {
-  fakeLazyLoad();
-}
-
-function fakeLazyLoad() {
-  for (x = done; x < posters + 1; x++) {
-    imageTransition('poster' + x);
+$(document).on('scroll', function(){
+  if ($(document).scrollTop() > $('#posters').height() - $('#first').height()/8) {
+    let count = ($(document).width()/2) / 200;
+    for (i = 0; i < count - 1; i++) {
+      addImage();
+    }
   }
-}
+});
 
-// Adds the css class 'show' when the height is approriate
-function imageTransition(id) {
-  let height = window.innerHeight;
-  let img1 = document.getElementById(id);
-  let img1value = img1.getBoundingClientRect().top + img1.height/2;
-
-  if (img1value < height) {
-    img1.classList.add('show');
-    img1.classList.remove('hide');
-    done++;
-  }
-}
-
-// Changes coffee image with random
-function changeImage() {
-  let rand = Math.floor((Math.random() * 7) + 1);
-  document.getElementById('coffee').src='../img/coffee/coffee' + rand + '.jpg';
-}
+function addImage() {
+  let number = Math.floor((Math.random() * 15) + 1);
+  let delay = Math.floor((Math.random() * 1000) + 500);
+  let img = $('<img class="child hide grow" src="../img/posters/poster' + number + '.jpg" />');
+  img.delay(delay).animate({"opacity": "1"}, 1000);
+  $('#posters').append(img);
+};
