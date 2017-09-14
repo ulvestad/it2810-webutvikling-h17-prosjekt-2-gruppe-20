c86dd4b8e89819d1545633aa68e998b4505e5491
Kristian Aurlien <kristian@aurlien.net>
2017-09-14 13:48:58 +0200

    use jquery

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,4 +1,4 @@
 function toggle_menu() {
-  const checked = document.getElementById("menu-toggle").checked;
-  document.getElementById("navbar-menu").classList.toggle('active');
+  const checked = $("#menu-toggle").checked;
+  $("#navbar-menu").classList.toggle('active');
 }
