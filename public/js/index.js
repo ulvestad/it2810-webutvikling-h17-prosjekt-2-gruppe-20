5ab1821e4698b857dd9248a683b3b919d4f65265
Andreajl <andreajl@stud.ntnu.no>
2017-09-21 10:47:53 +0200

    styling

--- public/js/index.js
+++ public/js/index.js
@@ -2,23 +2,25 @@ let counter = 0;
 let numberOfPosters = 15;
 
 $(document).ready(function() {
-  addSingleImage();
-  addSingleImage();
-  addSingleImage();
+  multiple(3);
 });
 
 $(document).on('scroll', function(){
   if ($(document).scrollTop() > $('#posters').height() && $(document).scrollTop() > 100) {
-    addSingleImage();
-    addSingleImage();
-    addSingleImage();
+    multiple(3);
   }
 });
 
+function multiple(m) {
+  for (var i = 0; i < m; i++) {
+    addSingleImage();
+  }
+}
+
 function addSingleImage() {
-  let id = (counter%numberOfPosters) + 1
+  let id = (counter%numberOfPosters) + 1;
   let img = $('<img class="child hide grow" src="../img/posters/poster' + id + '.jpg" />');
   img.delay(1000).animate({"opacity": "1"}, 2000);
   $('#posters').append(img);
   counter++;
-};
+}
