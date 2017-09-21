02837a27cf5e86b642faade2a814679c32c9e932
Andreajl <andreajl@stud.ntnu.no>
2017-09-21 11:51:10 +0200

    pretty

--- public/js/coffeemaker.js
+++ public/js/coffeemaker.js
@@ -1,16 +1,18 @@
 const coffee_per_cup = 7.5;
 const water_per_cup = 12.5;
 
+// Returns how much coffe and water you need based on input
 function calculate_coffee(n_cups) {
   let coffee = coffee_per_cup * n_cups;
   let water = water_per_cup * n_cups;
   return { coffee: coffee, water: water };
 };
 
+// Updates html with new values when input changes
 $('#nCups').on('input', function() {
   let cups = $('#nCups').val();
   let res = calculate_coffee(cups);
-  $("#result").html("<div>You need:<br/>" +
-    res.coffee + " grams of coffee<br/>" +
-    res.water + " ml of water");
+  $('#result').html('<div>You need:<br/>' +
+    res.coffee + ' grams of coffee<br/>' +
+    res.water + ' ml of water');
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,31 @@
+// variables to loop over all poster images
 let counter = 0;
 let numberOfPosters = 15;
 
+// Loads 3 images when page is ready
 $(document).ready(function() {
   multiple(3);
 });
 
+// When scrolling to the bottom of the page -> add more images
 $(document).on('scroll', function(){
   if ($(document).scrollTop() > $('#posters').height() && $(document).scrollTop() > 100) {
     multiple(3);
   }
 });
 
+// Adds multiple images
 function multiple(m) {
   for (var i = 0; i < m; i++) {
     addSingleImage();
   }
 }
 
+// Adds a single image element to the posters container
 function addSingleImage() {
   let id = (counter%numberOfPosters) + 1;
   let img = $('<img class="child hide grow" src="../img/posters/poster' + id + '.jpg" />');
-  img.delay(1000).animate({"opacity": "1"}, 2000);
+  img.delay(1000).animate({'opacity': '1'}, 2000);
   $('#posters').append(img);
   counter++;
 }
