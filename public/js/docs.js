747a53666231e866d4063d1f1fb435fadebdeddb
Andreajl <andreajl@stud.ntnu.no>
2017-09-24 19:16:31 +0200

    docs fix

--- public/js/docs.js
+++ public/js/docs.js
@@ -1,5 +1,5 @@
 const doc = "https://docs.google.com/document/d/e/2PACX-1vSEs-SjUaEw1fPU42jrH6WhDg1xohbZ-W4k0lQ4em5zcWG9gbRByfdpEnf8VGk5jxDsgdEj1fati54_/pub";
 $(document).ready(function(){
-  $("#docs").load('../files/docs.txt');
+  $("#docs").load('../files/docs.html');
 });
 
